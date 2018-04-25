85bc253363cfa50987c03b61ca18bd9a5dc7aaba
Mitchell <mdpung@gmail.com>
2018-04-25 14:52:26 -0500

    Updated Test.java to take user input from console.

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,12 @@
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Test {
+
     public static void main(String[] args) throws FileNotFoundException {
-        Crawler crawl = new Crawler("food");
+        System.out.println("Where would you like to start?");
+        String start = (new Scanner(System.in)).nextLine();
+        Crawler crawl = new Crawler(start);
         crawl.findPhilosophy();
     }
 }
